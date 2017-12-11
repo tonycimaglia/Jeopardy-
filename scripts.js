e1813e56db9345d28d40192b00ebf82f10518a18
tonycimaglia <tonycimaglia@gmail.com>
2017-12-11 18:11:23 -0500

    struggling to get score to update

--- scripts.js
+++ scripts.js
@@ -3,9 +3,6 @@ $(document).ready(function () {
 })
   
 // I need to create an object that stores all of my questions and answers.
-// I need to pull a question from that object and display it as a prompt to the user. 
-// I need a way to check and see if the user's answer (to lower case) matches the...
-// solution that I have stored for that question.
 
 const columnOneQuestions = {
   oneHundred: 'A coding language that was created by Brendan Ike in just 10 days.',
@@ -23,14 +20,24 @@ const columnOneAnswers = {
   fiveHundred: 'answer4',
 }
 
+let score = 0
+
+// I need to pull a question from that object and display it as a prompt to the user. 
+// I need a way to check and see if the user's answer (to lower case) matches the...
+// solution that I have stored for that question.
 
 $('p.100').click(function() {
   let usersAnswer = prompt(columnOneQuestions.oneHundred)
   if (usersAnswer.toLocaleLowerCase() === columnOneAnswers.oneHundred) {
     alert('Correct!!!')
-    $('span').html(+ 100)
+    score += 100 
   } else {
     alert("I'm Sorry, that's incorrect.")
-    $('span').html(- 100) // need to add or subtract the valaue of the question to the score
+    score -= 100 
   }
 })
+
+
+// need to add or subtract the valaue of the question to the score
+//after a user clicks on an element and answers a question, its contents ($ amount)
+// need to be removed from the dom.
