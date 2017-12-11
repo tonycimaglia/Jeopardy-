4b86d3dd4d59ba4570bae8760232994d9bb18e88
tonycimaglia <tonycimaglia@gmail.com>
2017-12-11 16:24:07 -0500

    made html skeleton, starting to psuedocode game logic

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
   console.log('the dom is loaded')
 })
+
+$('p').click(function() {
+  let usersAnswer = prompt(question from object)
+  let questionSolution = answer from object
+  if (usersAnswer === questionSolution) {
+    alert('Correct!!!')
+    $('h2').html(+ 100)
+  } else {
+    alert("I'm Sorry, that's incorrect.")
+    $('h2').html(+ 100)
+  }
+  
+// I need to create an object that stores all of my questions and answers.
+// I need a way to test to see if a user's answer from a prompt matches the answer
+// that corresponds to the question they selected
+// I need those 
+
+const columnOneQuestions
+
+})
