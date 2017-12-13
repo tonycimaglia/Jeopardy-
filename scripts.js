bc3d681d1dfeb8c966e46d8bd7fbdbf54d7e7a12
tonycimaglia <tonycimaglia@gmail.com>
2017-12-13 11:25:42 -0500

    clicking a cell now displays a question in the modal

--- scripts.js
+++ scripts.js
@@ -421,20 +421,20 @@ $('p.6-500').click(function() {
 
 
 // Get the modal
-var modal = document.getElementById('myModal');
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById('openModal');
+const openModal = document.getElementById('openModal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+const closeModal = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+openModal.onclick = function() {
+  modal.style.display = "block";
+  $('#questionContent').html(columnOneQuestions.first.question)
 }
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeModal.onclick = function() {
     modal.style.display = "none";
 }
