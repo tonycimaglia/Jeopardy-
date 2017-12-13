a0340b85cdecf307d803eefd98b5496d13ba74ff
tonycimaglia <tonycimaglia@gmail.com>
2017-12-13 09:42:00 -0500

    wrote out psuedocode for all of the modal logic that I will need.

--- scripts.js
+++ scripts.js
@@ -411,6 +411,15 @@ $('p.6-500').click(function() {
     columnSixQuestions.fifth.value)
 })
 
+// When the modal opens
+// User should have the option to answer or pass. 
+// Either way, the p element needs to be removed from the dom
+// if the user selects answer, they need to see the question and have a form
+// where they can enter or choose an answer
+// then, they need to see another modal telling them if they're right or wrong,
+// and then the score should update accordingly 
+
+
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -428,11 +437,4 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
